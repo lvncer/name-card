7294748a3660a68b654f1d78c2a7946f322291d7
lvncer <negimasa58@gmail.com>
2025-06-07 18:31:04 +0900

    修正: distディレクトリをgitignoreに追加 (#1)

--- dist/server.d.ts
+++
@@ -1,7 +0,0 @@
-interface ServerOptions {
-    port: number;
-    openBrowser: boolean;
-}
-export declare function startServer(markdownFile: string, options: ServerOptions): Promise<void>;
-export {};
-//# sourceMappingURL=server.d.ts.map
